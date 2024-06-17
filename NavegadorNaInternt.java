aa593d54c24bdb8bea58ed97a5d5de413588974d
medeirospatrik <medeirospatrik2@gmail.com>
2024-06-17 18:24:35 -0300

    Criando interfaces'

---
+++ NavegadorNaInternt.java
@@ -0,0 +1,5 @@
+public interface NavegadorNaInternt {
+    public void exibirPagina(String url);
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+}
---
+++ ReprodutorMusical.java
@@ -0,0 +1,5 @@
+public interface ReprodutorMusical {
+	public void copiar();
+	public void pausar();
+	public void selecionarMusica(String musica);
+}
